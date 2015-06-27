911843b31e23d8cbc9a2146844c20dc2692b1f76
Stuart Mathews <stumathews@gmail.com>
2015-06-27 11:28:12 +0100

    added unit tests skeleton

---
+++ windows/RenderClient/UnitTests/UnitTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+
+    [TestFixture]
+    public class UnitTests
+    {
+      [TestFixtureSetUp]
+      public void InitBeforeAnyTestsStart()
+      { /* ... */ }
+
+      [TestFixtureTearDown]
+      public void DisposeAfterAllTestsCompleted()
+      { /* ... */ }
+      [SetUp]
+      public void InitBeforeEachTest()
+      { /* ... */ }
+
+      [TearDown]
+      public void DisposeAfterEachTest()
+      { /* ... */ }
+
+      [Test]
+      [ExpectedException( typeof( InvalidOperationException ) )]
+      public void ExpectAnException()
+      { /* ... */ }
+
+      [Test]
+      public void Test1()
+      { /* ... */ }
+
+    }
+}
